bdb65924eb331d7ae59ea13a16fd9049be47a66c
pastre <pastre68@gmail.com>
2019-04-23 17:36:33 -0300

    Initial Commit

---
+++ Teste de segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teste de segues
+//
+//  Created by Bruno Pastre on 23/04/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Teste de seguesTests/Teste_de_seguesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Teste_de_seguesTests.swift
+//  Teste de seguesTests
+//
+//  Created by Bruno Pastre on 23/04/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+import XCTest
+@testable import Teste_de_segues
+
+class Teste_de_seguesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
